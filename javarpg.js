89a1139c75cd6bcc9532dffff05b51508b699284
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-08 16:33:57 +0100

    Update javarpg.js

--- javarpg.js
+++ javarpg.js
@@ -1,23 +1,37 @@
 var Attaque = document.getElementById('Attaque');
 var VieM1 = document.getElementById('HPM1');
-var HPM1 = 100
+var HPM1 = 100 ;
 var VieJ1 = 100 ;
 var AttaqueM = 10 ;
 var AttaqueJ1 = 10 ;
 VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
 
-Attaque.onclick function Att() {
+function Att(Perso) {
   VieM1.innerHTML = "B";
 }
 
-Défense.onclick function Def(){
+function Def(Perso){
   
 }
 
-Spécial.onclick function Spe(){
+function Spe(Perso){
   
 }
 
 Bulle1.onmouseover function InfoBulle1(){
   
+}
+
+while ((VieJ1 != 0) or (VieM1 != 0)) {
+  for i in range (0,4){
+    if Attaque.onclick {
+      Att(i)
+    }
+    else if Defense.onclick{
+      Def(i)
+    }
+    else if Special.onclick{
+      Spe(i)
+    }
+  }
 }
