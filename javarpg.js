cd925385ddd955fd86b21871ff036eb62af0fb76
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-08 15:57:24 +0100

    CombatRPG
    Combat style RPG en HTML/CSS/JS

---
+++ javarpg.js
@@ -0,0 +1,23 @@
+var Attaque = document.getElementById('Attaque');
+var VieM1 = document.getElementById('HPM1');
+var HPM1 = 100
+var VieJ1 = 100 ;
+var AttaqueM = 10 ;
+var AttaqueJ1 = 10 ;
+VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
+
+Attaque.onclick function Att() {
+  VieM1.innerHTML = "B";
+}
+
+Défense.onclick function Def(){
+  
+}
+
+Spécial.onclick function Spe(){
+  
+}
+
+Bulle1.onmouseover function InfoBulle1(){
+  
+}
