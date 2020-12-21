f66d8d32284abd2809d5d22afbc50ee34f49a372
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-21 16:07:57 +0100

    TentativeResolution
    InfoBulles ne marchent toujours pas...

--- javarpg.js
+++ javarpg.js
@@ -1,33 +1,80 @@
 var Attaque = document.getElementById('Attaque');
+var Defense = document.getElementById('Défense');
+var Special = document.getElementById('Spécial');
+
 var VieM1 = document.getElementById('HPM1');
+var VieM2 = document.getElementById('HPM2');
+var VieM3 = document.getElementById('HPM3');
+
 var HPM1 = 100 ;
 var HPM2 = 100 ;
 var HPM3 = 100 ;
+var AttaqueM = 10 ;
+
 var VieJ1 = 100 ;
 var VieJ2 = 80 ;
 var VieJ3 = 90 ;
 var VieJ4 = 120 ;
-var AttaqueM = 10 ;
+
+var Mana1 = 20;
+var Mana2 = 40;
+var Mana3 = 50;
+var Mana4 = 10;
+
 var AttaqueJ1 = 15 ;
 var AttaqueJ2 = 5 ;
 var AttaqueJ3 = 5 ;
 var AttaqueJ4 = 20 ;
+
+var Cible = 0;
+
 VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
 
-document.getElementById("Monstre1").addEventListener("mouseover", Bulle1Over);
-document.getElementById("Monstre1").addEventListener("mouseout", Bulle1Out);
 
-function Bulle1Over() {
-  document.getElementById("Bulle1").style.visibility= "visible" ;
+document.getElementById("Monstre1").onmouseover = function() {
+  document.getElementById("Bulle1").style.visibility= "visible" 
 }
 
-function Bulle1Out() {
-  document.getElementById("Bulle1").style.visibility = "hidden" ;
+document.getElementById("Monstre1").onmouseout = function() {
+  document.getElementById("Bulle1").style.visibility = "hidden" 
 }
 
+document.getElementById("Monstre2").onmouseover = function() {
+  document.getElementById("Bulle2").style.visibility= "visible" 
+}
 
+document.getElementById("Monstre2").onmouseout = function() {
+  document.getElementById("Bulle2").style.visibility = "hidden" 
+}
+
+document.getElementById("Monstre3").onmouseover = function() {
+  document.getElementById("Bulle3").style.visibility= "visible" 
+}
+
+document.getElementById("Monstre3").onmouseout = function() {
+  document.getElementById("Bulle3").style.visibility = "hidden" 
+}
+
+document.getElementById("Monstre1").onclick = function() {
+	if HPM1 > 0 {
+		Cible = 1
+	}
+}
+
+document.getElementById("Monstre2").onclick = function() {
+	if HPM2 > 0 {
+		Cible = 2
+	}
+}
+
+document.getElementById("Monstre3").onclick = function() {
+	if HPM3 > 0 {
+		Cible = 3
+	}
+}
 
 function Att(Perso) {
+	if (Perso == 1) {
 }
 
 function Def(Perso){
