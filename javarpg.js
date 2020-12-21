2909b7bd27595f2e1c28279349a274a324edf208
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-21 15:29:07 +0100

    RPG
    CSS et Html finalisé
    JS bug ?!

--- javarpg.js
+++ javarpg.js
@@ -1,13 +1,33 @@
 var Attaque = document.getElementById('Attaque');
 var VieM1 = document.getElementById('HPM1');
 var HPM1 = 100 ;
+var HPM2 = 100 ;
+var HPM3 = 100 ;
 var VieJ1 = 100 ;
+var VieJ2 = 80 ;
+var VieJ3 = 90 ;
+var VieJ4 = 120 ;
 var AttaqueM = 10 ;
-var AttaqueJ1 = 10 ;
+var AttaqueJ1 = 15 ;
+var AttaqueJ2 = 5 ;
+var AttaqueJ3 = 5 ;
+var AttaqueJ4 = 20 ;
 VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
 
+document.getElementById("Monstre1").addEventListener("mouseover", Bulle1Over);
+document.getElementById("Monstre1").addEventListener("mouseout", Bulle1Out);
+
+function Bulle1Over() {
+  document.getElementById("Bulle1").style.visibility= "visible" ;
+}
+
+function Bulle1Out() {
+  document.getElementById("Bulle1").style.visibility = "hidden" ;
+}
+
+
+
 function Att(Perso) {
-  VieM1.innerHTML = "B";
 }
 
 function Def(Perso){
@@ -15,10 +35,6 @@ function Def(Perso){
 }
 
 function Spe(Perso){
-  
-}
-
-Bulle1.onmouseover function InfoBulle1(){
   
 }
 
