63fdf10e5e45acc9e40914a9b67bab8bf6e6f98c
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-22 18:51:58 +0100

    Update javarpg.js

--- javarpg.js
+++ javarpg.js
@@ -99,82 +99,25 @@ function special() {
   nextStage();
 }
 
-// Boucle du combat
-while ((VieJ1 != 0 && VieJ2 != 0 && VieJ3 != 0 && VieJ4 !=0) || (VieM1 != 0 && VieM2 != 0 && VieM3 != 0)) {
-	//Actions Joueurs
-	//Variables Défense
-var ArmorJ1 = 2;
-var ArmorJ2 = 0;
-var ArmorJ3 = 1;
-var ArmorJ4 = 1;
-  for (let i=0; i<4 ; i++){
-    if (Attaque.onclick) {
-      Att(i);
+
+function updateUI() {
+  dialogue.innerHTML = "Au tour de " + characters[stage].name;
+  console.log("Au tour de " + characters[stage].name);
+  console.log(monstersElements.length);
+  for (var i = 0; i < monstersElements.length; i++)
+  {
+    if (monsters[i].health > 0) {
+      monstersElements[i].children[0].children[0].innerHTML = "Monstre " + i + " : " + monsters[i].health + " HP";
+      if (target == monsters[i]) {
+        monstersElements[i].style.opacity = "100%";
+      }
+      else {
+        monstersElements[i].style.opacity = "80%";
+      }
+    } else {
+      monstersElements[i].style.visibility = "hidden";
     }
-    else if (Defense.onclick){
-      Def(i);
-    }
-    else if (Special.onclick){
-      Spe(i);
-    }
-	if (i==1){
-		Dialogue.innerHTML = "Que doit faire Séréna?"
-	}
-	else if (i==2){
-		Dialogue.innerHTML = "Que doit faire Théo?"
-	}
-	else if (i==3){
-		Dialogue.innerHTML = "Que doit faire Jade?"
-	}
   }
-  //Fin Tour Joueur | Début Tour Monstre
-VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
-VieM2.innerHTML = "Monstre B : ", HPM2 ," Hp";
-VieM3.innerHTML = "Monstre C : ", HPM3 ," Hp";
-//En cas de mort d'un ou plusieurs monstres
-if (HPM1 == 0){
-	document.getElementById("Monstre1").style.visibility = "hidden";
-	AttaqueM -= 5
-}
-else if (HPM2 == 0){
-	document.getElementById("Monstre2").style.visibility = "hidden";
-	AttaqueM -= 5
-}
-else if (HPM3 == 0){
-	document.getElementById("Monstre3").style.visibility = "hidden";
-	AttaqueM -= 5
-}
-//Attaque des Monstres
-randomCible = Math.floor(Math.random() * TabJoueur.length) + 1;
-if (randomCible == 1){
-	VieJ1 -= AttaqueM - ArmorJ1;
-	VieJ1bis.innerHTML = VieJ1 ," Hp";
-}
-else if (randomCible == 2){
-	VieJ2 -= AttaqueM - ArmorJ2;
-	VieJ2bis.innerHTML = VieJ2 ," Hp";
-}
-else if (randomCible == 3){
-	VieJ3 -= AttaqueM - ArmorJ3;
-	VieJ3bis.innerHTML = VieJ3 ," Hp";
-}
-else if (randomCible == 4){
-	VieJ4 -= AttaqueM - ArmorJ4;
-	VieJ4bis.innerHTML = VieJ4 ," Hp";
-}
-VieJ1bis.innerHTML = VieJ1 ," Hp";
-VieJ2bis.innerHTML = VieJ2 ," Hp";
-VieJ3bis.innerHTML = VieJ3 ," Hp";
-VieJ4bis.innerHTML = VieJ4 ," Hp";
-tour += 1
 }
 
-//Condition de défaite
-if (VieJ1 != 0 && VieJ2 != 0 && VieJ3 != 0 && VieJ4 !=0) {
-	document.write("Vous avez perdu");
-}
-
-//Condition de victoire
-else if (VieM1 != 0 && VieM2 != 0 && VieM3 != 0) {
-	document.write("Vous avez gagné");
-}
+updateUI();
