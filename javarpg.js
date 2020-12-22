e7ebd096af4cca96bc689b52ff31153ba7fac398
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-22 11:43:26 +0100

    Update javarpg.js
    Début programmation d'un tour

--- javarpg.js
+++ javarpg.js
@@ -14,15 +14,23 @@ var AttaqueM = 10 ;
 
 //Variables Vie des joueurs
 var VieJ1 = 100 ;
+var VieJ1bis = document.getElementById('PV1');
 var VieJ2 = 80 ;
+var VieJ2bis = document.getElementById('PV2');
 var VieJ3 = 90 ;
+var VieJ3bis = document.getElementById('PV3');
 var VieJ4 = 120 ;
+var VieJ4bis = document.getElementById('PV4');
 
 //Variables Mana des joueurs
 var Mana1 = 20;
+var ManaJ1 = document.getElementById('Mana1');
 var Mana2 = 40;
+var ManaJ2 = document.getElementById('Mana2');
 var Mana3 = 50;
+var ManaJ3 = document.getElementById('Mana3');
 var Mana4 = 10;
+var ManaJ4 = document.getElementById('Mana4');
 
 //Variables Attaque des joueurs
 var AttaqueJ1 = 15 ;
@@ -33,53 +41,52 @@ var AttaqueJ4 = 20 ;
 //Autres variables utiles au bon déroulement du jeu
 var Cible = 0;
 var tour = 0;
-var Dialogue = document.getElementById
+var Dialogue = document.getElementById('Dialogue');
+const TabJoueur = [1,2,3,4]
+var randomCible = 0
 
-VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
-VieM2.innerHTML = "Monstre B : ", HPM1 ," Hp";
-VieM3.innerHTML = "Monstre C : ", HPM1 ," Hp";
 
 //Info-Bulles (ne fonctionne pas??)
 document.getElementById("Monstre1").onmouseover = function() {
-  document.getElementById("Bulle1").style.visibility= "visible" 
+  document.getElementById("Bulle1").style.visibility= "visible" ;
 }
 
 document.getElementById("Monstre1").onmouseout = function() {
-  document.getElementById("Bulle1").style.visibility = "hidden" 
+  document.getElementById("Bulle1").style.visibility = "hidden" ;
 }
 
 document.getElementById("Monstre2").onmouseover = function() {
-  document.getElementById("Bulle2").style.visibility= "visible" 
+  document.getElementById("Bulle2").style.visibility= "visible" ;
 }
 
 document.getElementById("Monstre2").onmouseout = function() {
-  document.getElementById("Bulle2").style.visibility = "hidden" 
+  document.getElementById("Bulle2").style.visibility = "hidden" ;
 }
 
 document.getElementById("Monstre3").onmouseover = function() {
-  document.getElementById("Bulle3").style.visibility= "visible" 
+  document.getElementById("Bulle3").style.visibility= "visible" ;
 }
 
 document.getElementById("Monstre3").onmouseout = function() {
-  document.getElementById("Bulle3").style.visibility = "hidden" 
+  document.getElementById("Bulle3").style.visibility = "hidden" ;
 }
 
 //Prise de la cible via click
 document.getElementById("Monstre1").onclick = function() {
 	if HPM1 > 0 {
-		Cible = 1
+		Cible = 1;
 	}
 }
 
 document.getElementById("Monstre2").onclick = function() {
 	if HPM2 > 0 {
-		Cible = 2
+		Cible = 2;
 	}
 }
 
 document.getElementById("Monstre3").onclick = function() {
 	if HPM3 > 0 {
-		Cible = 3
+		Cible = 3;
 	}
 }
 
@@ -103,21 +110,41 @@ function Spe(Perso){
 while ((VieJ1 != 0 && VieJ2 != 0 && VieJ3 != 0 && VieJ4 !=0) || (VieM1 != 0 && VieM2 != 0 && VieM3 != 0)) {
   for i in range (0,4){
     if Attaque.onclick {
-      Att(i)
+      Att(i);
     }
     else if Defense.onclick{
-      Def(i)
+      Def(i);
     }
     else if Special.onclick{
-      Spe(i)
+      Spe(i);
     }
   }
+VieM1.innerHTML = "Monstre A : ", HPM1 ," Hp";
+VieM2.innerHTML = "Monstre B : ", HPM2 ," Hp";
+VieM3.innerHTML = "Monstre C : ", HPM3 ," Hp";
+randomCible = Math.floor(Math.random() * TabJoueur.length) + 1;
+if (randomCible == 1){
+	VieJ1 -= AttaqueM ;
+	VieJ1bis.innerHTML = VieJ1 ," Hp";
+}
+else if (randomCible == 2){
+	VieJ2 -= AttaqueM ;
+	VieJ2bis.innerHTML = VieJ2 ," Hp";
+}
+else if (randomCible == 3){
+	VieJ3 -= AttaqueM ;
+	VieJ3bis.innerHTML = VieJ3 ," Hp";
+}
+else if (randomCible == 4){
+	VieJ4 -= AttaqueM ;
+	VieJ4bis.innerHTML = VieJ4 ," Hp";
+}
 }
 
 if (VieJ1 != 0 && VieJ2 != 0 && VieJ3 != 0 && VieJ4 !=0) {
-	document.write("Vous avez perdu")
+	document.write("Vous avez perdu");
 }
 
 else if (VieM1 != 0 && VieM2 != 0 && VieM3 != 0) {
-	document.write("Vous avez gagné")
+	document.write("Vous avez gagné");
 }
